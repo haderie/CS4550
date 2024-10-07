a23aa8e380170eeb8c13f24032531c526f33af04
haderie <haderie49@gmail.com>
2024-10-07 16:46:35 -0400

    added labs and minor edits

--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -5,58 +5,94 @@ import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
 import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
 import { IoSettingsOutline } from "react-icons/io5";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import NEU_logo from '../images/NEU_logo.png'
+import NEU_logo from "../images/NEU_logo.png";
 import { NewLineKind } from "typescript";
 
 export default function KanbasNavigation() {
   return (
+    <div
+      id="wd-kanbas-navigation"
+      style={{ width: 120 }}
+      className="list-group rounded-0 position-fixed
+           bottom-0 top-0 d-none d-md-block bg-black z-2"
+    >
+      <a
+        id="wd-neu-link"
+        target="_blank"
+        href="https://www.northeastern.edu/"
+        className="list-group-item bg-black border-0 text-center"
+      >
+        <img src={NEU_logo} width="75px" />
+      </a>
+      <br />
 
-      <div id="wd-kanbas-navigation" style={{ width: 120 }} 
-           className="list-group rounded-0 position-fixed
-           bottom-0 top-0 d-none d-md-block bg-black z-2">
-        <a id="wd-neu-link" target="_blank" 
-          href="https://www.northeastern.edu/"
-          className="list-group-item bg-black border-0 text-center">
-          <img src={NEU_logo} width="75px" /></a><br />
+      <Link
+        to="/Kanbas/Account"
+        id="wd-account-link"
+        className="list-group-item text-center border-0 bg-black text-white"
+      >
+        <FaRegCircleUser className="fs-1 text text-white" />
+        <br />
+        Account{" "}
+      </Link>
 
-        <Link to="/Kanbas/Account" id="wd-account-link"
-          className="list-group-item text-center border-0 bg-black text-white">
-          <FaRegCircleUser className="fs-1 text text-white" /><br />
-          Account </Link>
+      <Link
+        to="/Kanbas/Dashboard"
+        id="wd-dashboard-link"
+        className="list-group-item text-center border-0
+                     bg-white text-danger"
+      >
+        <AiOutlineDashboard className="fs-1 text-danger" />
+        <br />
+        Dashboard{" "}
+      </Link>
 
-        <Link to="/Kanbas/Dashboard" id="wd-dashboard-link"
-          className="list-group-item text-center border-0
-                     bg-white text-danger">
-          <AiOutlineDashboard className="fs-1 text-danger" /><br />
-          Dashboard </Link>
-
-        <Link to="/Kanbas/Dashboard" id="wd-course-link"
-          className="list-group-item text-white
-                     bg-black text-center border-0">
-          <LiaBookSolid className="fs-1 text-danger" /><br />
-          Courses </Link>
-        {/* complete styling the rest of the links */}
-
-        <Link to="/Kanbas/Dashboard" id="wd-course-link"
-          className="list-group-item text-white
-                     bg-black text-center border-0">
-          <IoCalendarOutline className="fs-1 text-danger" /><br />
-          Calendar </Link>
-
-        <Link to="/Kanbas/Dashboard" id="wd-course-link"
+      <Link
+        to="/Kanbas/Dashboard"
+        id="wd-course-link"
         className="list-group-item text-white
-                    bg-black text-center border-0">
-        <FaInbox className="fs-1 text-danger" /><br />
-        Inbox </Link>
+                     bg-black text-center border-0"
+      >
+        <LiaBookSolid className="fs-1 text-danger" />
+        <br />
+        Courses{" "}
+      </Link>
+      {/* complete styling the rest of the links */}
 
-        <Link to="/Labs" id="wd-course-link"
-          className="list-group-item text-white
-                     bg-black text-center border-0">
-          <IoSettingsOutline className="fs-1 text-danger" /><br />
-          Labs </Link>
-      </div>
-  
+      <Link
+        to="/Kanbas/Dashboard"
+        id="wd-course-link"
+        className="list-group-item text-white
+                     bg-black text-center border-0"
+      >
+        <IoCalendarOutline className="fs-1 text-danger" />
+        <br />
+        Calendar{" "}
+      </Link>
+
+      <Link
+        to="/Kanbas/Dashboard"
+        id="wd-course-link"
+        className="list-group-item text-white
+                    bg-black text-center border-0"
+      >
+        <FaInbox className="fs-1 text-danger" />
+        <br />
+        Inbox{" "}
+      </Link>
+
+      <Link
+        to="/Labs"
+        id="wd-course-link"
+        className="list-group-item text-white
+                     bg-black text-center border-0"
+      >
+        <IoSettingsOutline className="fs-1 text-danger" />
+        <br />
+        Labs{" "}
+      </Link>
+    </div>
   );
 }

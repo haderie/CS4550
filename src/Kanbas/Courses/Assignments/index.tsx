e30b50173642dd6cd282e3f6b5dff89593b320bf
haderie <haderie49@gmail.com>
2024-10-08 17:46:58 -0400

    final a2

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -10,48 +10,42 @@ export default function Assignments() {
   return (
     <div
       id="wd-assignments"
-      className="wd-module list-group-item p-0 mb-5 fs-5 border-gray"
+      className="list-group-item p-0 mb-5 fs-5 border-gray"
     >
       <div className="mb-5 mt-4 d-flex">
         <SearchBar />
         <AssignmentBar />
       </div>
+
       <div
         id="wd-assignments-title"
-        className="wd-title align-items-center p-3 ps-2 bg-secondary"
+        className="wd-title d-flex align-items-center p-3 ps-2 bg-secondary"
       >
         <BsGripVertical />
-        <b>ASSIGNMENTS</b>
-        <div className="float-end">
-          <label className="form-label border rounded-pill ">
-            40% of Total{" "}
+        <b className="ms-2">ASSIGNMENTS</b>
+        <div className="ms-auto d-flex align-items-center">
+          <label className="form-label border rounded-pill px-2 me-2">
+            40% of Total
           </label>
-          <BsPlus style={{ fontSize: "1.5em" }} />
+          <BsPlus style={{ fontSize: "1.5em" }} className="me-2" />
           <IoEllipsisVertical className="fs-4" />
         </div>
       </div>
 
       <ul id="wd-assignment-list" className="list-group rounded-0">
-        <li className="wd-lesson wd-assignment-list-item list-group-item p-3 ps-1">
+        <li className="wd-assignment-list-item list-group-item p-3 ps-1">
           <a
-            className="wd-assignment-link"
+            className="wd-assignment-link d-flex justify-content-between align-items-center text-decoration-none text-dark"
             href="#/Kanbas/Courses/1234/Assignments/123"
-            style={{
-              textDecoration: "none",
-              color: "inherit",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
           >
-            <div style={{ display: "flex", alignItems: "center" }}>
+            <div className="d-flex align-items-center">
               <AssignmentControlButtons />
-              <div style={{ marginLeft: "10px" }}>
+              <div className="ms-2">
                 <h4>
                   <b>A1</b>
                 </h4>
                 <h5>
-                  <span style={{ color: "red" }}> Multiple Modules </span> |{" "}
+                  <span className="text-danger"> Multiple Modules </span> |{" "}
                   <b>Not available</b> Until May 6 at 12:00 AM |
                 </h5>
                 <h5>
@@ -63,30 +57,47 @@ export default function Assignments() {
           </a>
         </li>
 
-        <li className="wd-lesson wd-assignment-list-item list-group-item p-3 ps-1">
+        <li className="wd-assignment-list-item list-group-item p-3 ps-1">
           <a
-            className="wd-assignment-link"
+            className="wd-assignment-link d-flex justify-content-between align-items-center text-decoration-none text-dark"
             href="#/Kanbas/Courses/1234/Assignments/123"
-            style={{
-              textDecoration: "none",
-              color: "inherit",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
           >
-            <div style={{ display: "flex", alignItems: "center" }}>
+            <div className="d-flex align-items-center">
               <AssignmentControlButtons />
-              <div style={{ marginLeft: "10px" }}>
+              <div className="ms-2">
                 <h4>
                   <b>A2</b>
                 </h4>
                 <h5>
-                  <span style={{ color: "red" }}> Multiple Modules </span> |{" "}
-                  <b>Not available</b> Until May 6 at 12:00 AM |
+                  <span className="text-danger"> Multiple Modules </span> |{" "}
+                  <b>Not available</b> Until April 15 at 12:00 AM |
                 </h5>
                 <h5>
-                  <b>Due</b> May 13 at 11:59 PM | 100 pts
+                  <b>Due</b> Jun 13 at 11:59 PM | 100 pts
+                </h5>
+              </div>
+            </div>
+            <LessonControlButtons />
+          </a>
+        </li>
+
+        <li className="wd-assignment-list-item list-group-item p-3 ps-1">
+          <a
+            className="wd-assignment-link d-flex justify-content-between align-items-center text-decoration-none text-dark"
+            href="#/Kanbas/Courses/1234/Assignments/123"
+          >
+            <div className="d-flex align-items-center">
+              <AssignmentControlButtons />
+              <div className="ms-2">
+                <h4>
+                  <b>A3</b>
+                </h4>
+                <h5>
+                  <span className="text-danger"> Multiple Modules </span> |{" "}
+                  <b>Not available</b> Until Jun 10 at 12:00 AM |
+                </h5>
+                <h5>
+                  <b>Due</b> Jun 20 at 11:59 PM | 100 pts
                 </h5>
               </div>
             </div>
